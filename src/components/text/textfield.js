0dbafb6b6bc952fd1a30f4a018fbf7cfe111bd07
zenokan-4ik <zenokan4ik@gmail.com>
2025-02-18 23:41:36 +0300

    fix text inputs
    meow

--- src/components/text/textfield.js
+++ src/components/text/textfield.js
@@ -3,7 +3,7 @@ import './styles.css'
 
 const TextField = ({onChange, type, value, about, placeholder}) => {
   return (
-    <div style={{justifyContent: 'left', minWidth: '100%'}}>
+    <div style={{alignContent: 'left', minWidth: '100%'}}>
       <p className="about">{about}</p>
       <input className='base-textfield' type={type} value={value} onChange={onChange} placeholder={placeholder}/>
     </div>
