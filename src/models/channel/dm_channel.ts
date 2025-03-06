5eda31e0a143e1047fc0bb2d245f3593681e0723
Jason <ivandomogatskij@gmail.com>
2025-03-06 15:30:19 +0300

    smth

--- src/models/channel/dm_channel.ts
+++ src/models/channel/dm_channel.ts
@@ -1,7 +1,6 @@
 import { Channel } from "./channel";
 import { User } from "../user";
-import { Message } from '../message'
 
-interface DMChannel extends Channel {
+export interface DMChannel extends Channel {
     user: User;
 }
--- src/pages/home/components/sections/dm_channels/dm_channels.jsx
+++ src/pages/home/components/sections/dm_channels/dm_channels.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import { React, useState } from "react";
 import TextField from "../../../../../components/text/textfield";
 import "./styles.css";
 import Dm from "../../dm/dm";
-import PlusSvg from "../../../../../images/plus.svg";
+import { ReactComponent as PlusSvg } from "../../../../../images/plus.svg";
 
 const DmChannelsSection = ({ dms }) => {
+    const [foundDms, setFoundDms] = useState(dms);
+    const [useFound, setUseFound] = useState(false);
+
     return (
         <div className="dm-section">
             <div className="search">
                 <TextField
                     type="text"
                     placeholder="find or start a conversation"
+                    onChange={(e) => {
+                        setUseFound(e.target.value.length !== 0);
+
+                        setFoundDms(
+                            dms.filter((dm) =>
+                                dm.user.display_name
+                                    .toLowerCase()
+                                    .includes(e.target.value.toLowerCase())
+                            )
+                        );
+                    }}
                 ></TextField>
             </div>
-            <div className="title"
-            >
-                DIRECT MESSAGES
-                <img src={PlusSvg} />
-            </div>
-            {dms.map((data) => (
-                <Dm data={data} />
-            ))}
+            {!useFound && (
+                <div className="title">
+                    DIRECT MESSAGES
+                    <PlusSvg />
+                </div>
+            )}
+
+            {foundDms.length !== 0 ? (
+                foundDms.map((data) => <Dm data={data} />)
+            ) : (
+                <div className="empty-result">Nothing found</div>
+            )}
         </div>
     );
 };
