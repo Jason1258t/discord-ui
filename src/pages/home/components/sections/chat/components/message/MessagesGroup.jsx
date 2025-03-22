506e130920aa8d7af09f56c819fb701bbc8e9739
Jason <ivandomogatskij@gmail.com>
2025-03-22 21:23:16 +0300

    meow

--- src/pages/home/components/sections/chat/components/message/MessagesGroup.jsx
+++ src/pages/home/components/sections/chat/components/message/MessagesGroup.jsx
@@ -3,7 +3,7 @@ import Message from "./Message";
 
 const MessagesGroup = ({ messages }) => {
     return (
-        <div >
+        <div style={{marginBottom: 8}}>
             <Message data={messages[0]} showInfo={true} />
             {messages.slice(1).map((e) => (
                 <Message data={e} showInfo={false} />
